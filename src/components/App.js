e65ff3622c6ff5373244cc7bdb109b92e37b2446
shoudaos <shoudaos92@gmail.com>
2019-03-18 13:38:40 +0300

    fix(Sidebar): temporarily disable auth checking

--- src/components/App.js
+++ src/components/App.js
@@ -11,24 +11,24 @@ import LayoutComponent from '../components/Layout';
 import LoginComponent from '../pages/login';
 import '../styles/theme.scss';
 
-const PrivateRoute = ({ component, ...rest }) => {
-  return ( // eslint-disable-line
-    <Route
-      {...rest} render={props => (
-      localStorage.getItem('id_token') ? (
-        React.createElement(component, props)
-      ) : (
-        <Redirect
-          to={{
-            pathname: '/login',
-            state: { from: props.location }, // eslint-disable-line
-          }}
-        />
-      )
-    )}
-    />
-  );
-};
+// const PrivateRoute = ({ component, ...rest }) => {
+//   return ( // eslint-disable-line
+//     <Route
+//       {...rest} render={props => (
+//       localStorage.getItem('id_token') ? (
+//         React.createElement(component, props)
+//       ) : (
+//         <Redirect
+//           to={{
+//             pathname: '/login',
+//             state: { from: props.location }, // eslint-disable-line
+//           }}
+//         />
+//       )
+//     )}
+//     />
+//   );
+// };
 
 class App extends React.PureComponent {
   render() {
@@ -37,7 +37,8 @@ class App extends React.PureComponent {
         <Switch>
           <Route path="/" exact render={() => <Redirect to="/app/dashboard" />} />
           <Route path="/app" exact render={() => <Redirect to="/app/dashboard" />} />
-          <PrivateRoute path="/app" component={LayoutComponent} />
+           {/*ToDo return Private Route*/}
+          <Route path="/app" component={LayoutComponent} />
           <Route path="/login" exact component={LoginComponent} />
           <Route path="/error" exact component={ErrorPage} />
           <Redirect from="*" to="/app/dashboard" />
