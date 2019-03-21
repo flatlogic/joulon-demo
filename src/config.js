1ba671dd9875e5ada980924f113ae3ad0e45d7ef
shoudaos <shoudaos92@gmail.com>
2019-03-21 15:14:41 +0300

    fix(App config): fix production https protocol

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === "development") {
   host = "http://localhost";
   port = 8080;
 } else {
-  host = "http://joulon-flatlogic-api.herokuapp.com";
+  host = "https://joulon-flatlogic-api.herokuapp.com";
   port = "";
 }
 
